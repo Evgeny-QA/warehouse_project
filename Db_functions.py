4ccd12ceeb75409ecc3c31b42d3f84f78a0c380c
Evgen-QA <evgenygmgqa@gmail.com>
2024-01-28 11:30:59 +0300

    some corrections

--- Db_functions.py
+++ Db_functions.py
@@ -6,18 +6,18 @@ class DataBase:
         self.db_file = 'Warehouses_db.db'
         self.db = sql.connect(self.db_file)
 
-    def log_in(self, login, password):
+    def log_in(self, login):
         """Вход в приложение базы пользователем
-        :param login: логин, password: пароль
-        :return: True - вход успешно выполнен, False - неверный логин или пароль"""
+        :param login: логин
+        :return: Возвращает пароль, если пароль не верный - None"""
         try:
             with self.db:
                 cursor = self.db.cursor()
-                cursor.execute('''SELECT login, password
-                                  FROM Users
-                                  WHERE login = ? AND password = ?''', [login, password])
-                result = cursor.fetchone()
-                return True if result is not None else False
+                cursor.execute('''SELECT password 
+                                  FROM users 
+                                  WHERE login=?''', [login])
+                password = cursor.fetchone()
+                return password
         except sql.Error as error:
             print(f"Произошла ошибка: {error}")
             return False
@@ -30,37 +30,54 @@ class DataBase:
                 cursor = self.db.cursor()
                 cursor.execute('''SELECT warehouse_name
                                   FROM Warehouses''')
-                result = cursor.fetchall()
-                return [i[0] for i in result]
+                names = cursor.fetchall()
+                return [i[0] for i in names]
         except sql.Error as error:
             print(f"Произошла ошибка: {error}")
             return False
 
-    def get_warehouse_info(self, warehouse_name):
-        """Получение информации о товарах на складе
-        :param warehouse_name: название склада
-        :return: возвращает список с кортежами каждого товара (id, категория, название, количество, ед. изм.,
+    def get_all_goods_from_warehouses(self):
+        """Получение информации о всех товарах на всех складах
+        :return: возвращает все товары на всех складах (склад, категория, название, количество, ед. изм.,
                  цена, дата изготовления, годен до, описание, артикул, путь к фото)"""
         try:
             with self.db:
                 cursor = self.db.cursor()
-                cursor.execute('''SELECT G.id, category_name, good_name, amount, measure_unit, price, time_start, 
+                cursor.execute('''SELECT warehouse_name, category_name, good_name, amount, measure_unit, price, 
+                                         time_start, time_to_end, description, article_number, image
+                                  FROM Goods G JOIN Warehouses W ON G.warehouse_id = W.id 
+                                               JOIN Categories C ON G.category_id = C.id''')
+                info = cursor.fetchall()
+                return info
+        except sql.Error as error:
+            print(f"Произошла ошибка: {error}")
+            return False
+
+    def get_goods_from_warehouse(self, warehouse_name):
+        """Получение информации о товарах на определенном складе
+        :param warehouse_name: название склада
+        :return: возвращает список с кортежами каждого товара (категория, название, количество, ед. изм.,
+                 цена, дата изготовления, годен до, описание, артикул, путь к фото)"""
+        try:
+            with self.db:
+                cursor = self.db.cursor()
+                cursor.execute('''SELECT category_name, good_name, amount, measure_unit, price, time_start, 
                                          time_to_end, description, article_number, image
                                   FROM Goods G JOIN Warehouses W ON G.warehouse_id = W.id 
                                                JOIN Categories C ON G.category_id = C.id
                                   WHERE warehouse_name = ?''', [warehouse_name])
-                result = cursor.fetchall()
-                return result
+                info = cursor.fetchall()
+                return info
         except sql.Error as error:
             print(f"Произошла ошибка: {error}")
             return False
 
     def add_new_good_into_db(self, good_info):
         """Добавление нового товара на склад
-        :param good_info: [название склада (текущий) (str), категория (str), название товара (str),
+        :param good_info: [название склада (str), категория (str), название товара (str),
                            количество (int), ед. изм. (str), цена (int), дата изготовления (str),
                            годен до (str), описание (str), артикул (int), путь к фото (str)]
-        :return: True - товар добавлен, False - описание ошибки"""
+        :return: True - товар добавлен"""
         try:
             with self.db:
                 '''Проверка на наличие категории/добавление и получение id категории'''
@@ -76,9 +93,6 @@ class DataBase:
                                       VALUES (?)''', [good_info[1]])
                     cursor.execute('''SELECT MAX(id)
                                       FROM Categories''')
-                    # cursor.execute('''SELECT id
-                    #                   FROM Categories
-                    #                   WHERE category_name = ?''', [good_info[1]])
                     good_info[1] = cursor.fetchone()
                 cursor.execute('''SELECT id
                                   FROM Warehouses
@@ -87,7 +101,7 @@ class DataBase:
                 good_info[0], good_info[1] = good_info[1][0], good_info[0][0]  # из кортежа в число
 
                 cursor.execute('''INSERT INTO Goods(warehouse_id, category_id, good_name, amount, measure_unit,
-                                  price, time_start, time_to_end, description, article_number, image)
+                                                    price, time_start, time_to_end, description, article_number, image)
                                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', good_info)
                 return cursor.rowcount > 0
         except sql.Error as error:
@@ -95,4 +109,4 @@ class DataBase:
             return False
 
 
-#DataBase().add_new_good_into_db(["Запчасть плюс", "Проgdgdдукffaт;ы", "1", 1, "1", 1, "1", "1", "1", 1, "1"])
+DataBase().add_new_good_into_db(["Запчасть плюс", "Проgdgdдукffaт;ы", "1", 1, "1", 1, "1", "1", "1", 1, "1"])
